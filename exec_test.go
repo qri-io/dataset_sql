5751f366f8a3131306e00ca2834e93040d19baa1
Brendan O'Brien <brendanobrienesq@gmail.com>
2016-11-17 18:39:18 -0500

    added a few more tests

--- exec_test.go
+++ exec_test.go
@@ -39,7 +39,14 @@ func TestSelectFields(t *testing.T) {
 		o.NumRecords = 10
 	})
 
-	domain := &TestDomain{datasets: []*dataset.Dataset{ds}}
+	dsTwo := dataset_generate.RandomDataset(func(o *dataset_generate.RandomDatasetOpts) {
+		o.Name = "select_test_two"
+		o.Address = dataset.NewAddress("select_test_two")
+		o.Fields = []*dataset.Field{created, title, views, rating, notes}
+		o.NumRecords = 10
+	})
+
+	domain := &TestDomain{datasets: []*dataset.Dataset{ds, dsTwo}}
 
 	cases := []struct {
 		statement string
@@ -47,10 +54,11 @@ func TestSelectFields(t *testing.T) {
 		fields    []*dataset.Field
 		numRows   int
 	}{
-		{"select * from select_test", nil, []*dataset.Field{created, title, views, rating, notes}, 10},
-		{"select created, title, views, rating, notes from select_test", nil, []*dataset.Field{created, title, views, rating, notes}, 10},
-		{"select created from select_test limit 5", nil, []*dataset.Field{created}, 5},
-		{"select created from select_test limit 1 offset 1", nil, []*dataset.Field{created}, 1},
+		// {"select * from select_test", nil, []*dataset.Field{created, title, views, rating, notes}, 10},
+		// {"select created, title, views, rating, notes from select_test", nil, []*dataset.Field{created, title, views, rating, notes}, 10},
+		// {"select select_test->created from select_test limit 5", nil, []*dataset.Field{created}, 5},
+		// {"select created from select_test limit 1 offset 1", nil, []*dataset.Field{created}, 1},
+		{"select * from select_test, select_test_two", nil, []*dataset.Field{created, title, views, rating, notes, created, title, views, rating, notes}, 20},
 		// {"select 1 from select_test", nil, []*dataset.Field{&dataset.Field{Name: "result", Type: datatype.Integer}}, 1},
 	}
 
@@ -90,11 +98,16 @@ func TestSelectFields(t *testing.T) {
 			continue
 		}
 
-		// fmt.Println(records)
-
 		if len(records) != c.numRows {
 			t.Errorf("case %d result count mismatch. expected: %d, got: %d", i, c.numRows, len(records))
 			continue
 		}
+
+		// table := tablewriter.NewWriter(os.Stdout)
+		// table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+		// table.SetCenterSeparator("|")
+		// table.SetHeader(results.FieldNames())
+		// table.AppendBulk(records)
+		// table.Render()
 	}
 }
